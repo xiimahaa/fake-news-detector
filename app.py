8d625fb24b239ebc6e976a31d6795786300b55f9
Maha Albash <165519657+xiimahaa@users.noreply.github.com>
2026-05-10 01:44:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import re
 import string
 import nltk
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 import os
 import gdown
+
+# Download model files
 
 MODEL_URL = "https://drive.google.com/uc?id=1YTlVel8LcbAUYOyqYPCiEiu_kwfD7QS4"
 VECTORIZER_URL = "https://drive.google.com/uc?id=1iMahPaNtDGSXsW4Atd00dKs7rz6Aws8I"
@@ -19,15 +22,22 @@ if os.path.exists("tfidf_vectorizer.pkl"):
 gdown.download(MODEL_URL, "fake_news_rf_model.pkl", quiet=False)
 gdown.download(VECTORIZER_URL, "tfidf_vectorizer.pkl", quiet=False)
 
+# Load model
+model = joblib.load("fake_news_rf_model.pkl")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
+
 app = Flask(__name__)
 
-# Text preprocessing
-from nltk.corpus import stopwords
+
+nltk.download("stopwords", quiet=True)
+nltk.download("wordnet", quiet=True)
 
 lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words('english'))
+stop_words = set(stopwords.words("english"))
 
+# Text cleaning
 def clean_text(text):
+
     text = text.lower()
 
     # Remove URLs
@@ -39,9 +49,10 @@ def clean_text(text):
     # Remove numbers
     text = re.sub(r'\d+', '', text)
 
-    # Tokenization + stopword removal + lemmatization
+    # Tokenize
     words = text.split()
 
+    # Remove stopwords + lemmatize
     words = [
         lemmatizer.lemmatize(word)
         for word in words
@@ -67,15 +78,19 @@ def predict():
 
     prediction = model.predict(transformed_news)[0]
 
-
-    probability = model.predict_proba(transformed_news)
-
-    confidence = round(max(probability[0]) * 100, 2)
+    try:
+        probability = model.predict_proba(transformed_news)
+        confidence = round(max(probability[0]) * 100, 2)
+    except:
+        confidence = "N/A"
 
     if prediction == 0:
         result = "Fake News"
     else:
         result = "Real News"
+
+    print("Prediction:", prediction)
+    print("Confidence:", confidence)
 
     return render_template(
         'index.html',
@@ -86,6 +101,5 @@ def predict():
 # Run app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
     
     
